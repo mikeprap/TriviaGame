d7ce32096c8dce6466d4105f18f21c6faa82b371
Mike Prap <michaelpraplaski@yahoo.com>
2019-09-10 15:00:17 -0400

    added pseudo code from the homework starter

---
+++ assets/javascript/game.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+var game = {
+    // set the object values
+    // question: questions
+    // count: countStartNumber
+    // currentQuestion = 0;
+    // correct = 0;
+    // incorrect = 0;
+    // Countdown function
+    countDown: function() {
+         // decrement counter
+        // display counter in on page
+        // run timer function
+    },
+    // loadQuestion: function
+    loadQuestion: function() {
+        // set timer variable
+        // add question dynamically
+        // for loop to go through questions
+    },
+        
+    // nextQuestion: function
+    nextQuestion: function() {
+        // set countdown
+        // display counter
+        // increment currentQuestion
+        //run loadQuestion function
+    },
+        
+    // timeUp: function
+    timeUp: function() {
+        // clearInterval
+        // display on page
+        // conditional - if your in current question or next question
+    },
+        
+    // results: function
+    results: function () {
+        // clear interval
+        // display results
+    },
+    //clicked: function ()
+    clicked: function () {
+    },
+
+    // answerInCorrectly: function
+    answerInCorrectly: function () {
+        // increment incorrect
+        // display the right answer
+        // condition to either go to results or go to next question
+    },
+        
+    // answeredCorrect: function
+    answerCorrect: function() {
+        // increment correct
+        // display that they are correct
+    },
+        
+    // reset: function
+    reset: function () {
+        
+    }
+    // Click events
+        //$(document).on("click") that calls the game function
+        //$(document).on("click") that calls the clicked function
+        //$(document).on("click") that calls the loadQuestion function
+    
+
+}
